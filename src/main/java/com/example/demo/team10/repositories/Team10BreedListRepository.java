e3f5e5c560cd6e1e0b3ce43d1d8273b34d72dff1
fukuoka <hi_fukuoka@bold.ne.jp>
2026-05-20 18:01:32 +0900

    com1

--- src/main/java/com/example/demo/team10/repositories/Team10BreedListRepository.java
+++ src/main/java/com/example/demo/team10/repositories/Team10BreedListRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.team10.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,19 +9,9 @@ import com.example.demo.team10.entity.Team10BreedList;
 
 @Repository
 public interface Team10BreedListRepository extends JpaRepository<Team10BreedList,String>{
-<<<<<<< HEAD
-	//List<Team10BreedList> findAll();
+	List<Team10BreedList> findAll();
 	
 	//List<Team10BreedList> findByBreedNameContaining(String breedId);
 	
 	//List<Team10BreedList> findBySizeId(String sizeId);
-=======
-//	List<Team10BreedList> findAll();
-//	
-//	List<Team10BreedList> findByBreed_NameContaining(String breedId);
-//	
-//	List<Team10BreedList> findBySizeId(String sizeId);
->>>>>>> 89ac87f23b905e00098835dfe07a056f6880ea35
-	
-	
 }
