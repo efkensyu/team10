86965d54e1b2d78f8b35d7b615e8726a4a5cebae
fukuoka <hi_fukuoka@bold.ne.jp>
2026-05-22 15:46:12 +0900

    f_insert

--- src/main/java/com/example/demo/team10/petregister/Team10PetRegisterController.java
+++ src/main/java/com/example/demo/team10/petregister/Team10PetRegisterController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.team10.entity.Team10BreedList;
 import com.example.demo.team10.entity.Team10PetRegister;
+import com.example.demo.team10.repositories.Team10BreedListRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,21 +20,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Team10PetRegisterController {
 	private final Team10PetRegisterService service;
+	private final Team10BreedListRepository breedRepository;
+	
 
-	@GetMapping("/register")
+	@GetMapping("/team10/petregister")
 	public String index(Model model) {
-		//List<Team10PetRegister> userDataList = repository.findAll();
-		//model.addAttribute("userDataList", userDataList);
+		List<Team10BreedList> breedList = breedRepository.findAll();
+		model.addAttribute("breedList", breedList);
 		return "team10/petregister/Team10PetRegisterIn";
 	}
 
-	@PostMapping("/register")
+	@PostMapping("/team10/petregister")
 	public String register(Team10PetRegister pet, @RequestParam("imageFile") MultipartFile imageFile, Model model)
 			throws IOException {
 		if (!imageFile.isEmpty()) {
 			String fileName = service.saveImage(imageFile);
 			pet.setPetImage(fileName);
 		}
+		//ダミーパスワード
+		pet.setPetPass("dummy_password");
 		//リポジトリ実行
 		service.saveTeam10PetRegister(pet);
 		List<Team10PetRegister> DataList = service.getAllTeam10PetRegister();
