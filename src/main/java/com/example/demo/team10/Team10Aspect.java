81452fdbd3169688cc376580b6427e9d3cae0606
R.matsumoto <r_matsumoto@bold.ne.jp>
2026-05-22 14:00:51 +0900

    html fix

--- src/main/java/com/example/demo/team10/Team10Aspect.java
+++ src/main/java/com/example/demo/team10/Team10Aspect.java
@@ -2,48 +2,34 @@ package com.example.demo.team10;
 
 import java.util.Arrays;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class Team10Aspect {
-	@Before("execution(* com.example.demo.team10.*.*.index(..))")
-	public void beforeLog(JoinPoint jp) {
-		System.out.println("[Before]indexメソッド開始：" + jp.getSignature());
-		System.out.println("送ったデータ：" + Arrays.toString(jp.getArgs()));
-	}
-	
-	@After("execution(* com.example.demo.team10.*.*.index(..))")
-	public void afterLog(JoinPoint jp) {
-		System.out.println("[After]indexメソッド開始：" + jp.getSignature());
-	}
-	
-	@Around("execution(* com.example.demo.team10.*.*.index(..))")
-	public Object aroundLog(ProceedingJoinPoint jp) throws Throwable  {
-		System.out.println("[Around]indexメソッド開始：" + jp.getSignature());
-		Object result = jp.proceed(); //indexを実行してる
-		//indexがkensyu5/Aop1_1をリターンしてる
-		System.out.println("[Around]indexメソッド終了：" + jp.getSignature());
-		
-		return result;//ここでkensyu5/1_1のページを開いて元に戻る
-		//return "kensyu5/Test";
-	}
-	
-	@AfterReturning("execution(* com.example.demo.team10.*.index(..))")
-	public void returningLog(JoinPoint jp) {
-		System.out.println("[AfterReturning]indexメソッド正常終了：" + jp.getSignature());
-	}
-	
-	@AfterThrowing("execution(* com.example.demo.team10.*.index(..))")
-	public void throwingLog(JoinPoint jp) {
-		System.out.println("[AfterThrowing]indexメソッド異常終了：" + jp.getSignature());
-	}
+	@Around("execution(* com.example.demo.team10..*(..))")
+    public Object logging(ProceedingJoinPoint pjp) throws Throwable {
+
+        String className  = pjp.getTarget().getClass().getSimpleName();
+        String methodName = pjp.getSignature().getName();
+        Object[] args     = pjp.getArgs();
+
+        System.out.println("=== [開始] " + className + "." + methodName + " ===");
+        System.out.println("  引数: " + Arrays.toString(args));
+
+        try {
+            Object result = pjp.proceed();
+            System.out.println("  戻り値: " + result);
+            System.out.println("=== [終了] " + className + "." + methodName + " ===");
+            return result;
+
+        } catch (Throwable e) {
+            System.out.println("  例外発生: " + e.getClass().getSimpleName());
+            System.out.println("  メッセージ: " + e.getMessage());
+            throw e;
+        }
+    }
 }
